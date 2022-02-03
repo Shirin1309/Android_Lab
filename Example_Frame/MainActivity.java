504d29077d517bf719102f6190c227aecc4a2d52
Shirin1309 <77880448+Shirin1309@users.noreply.github.com>
2022-02-03 02:27:01 -0800

    Add files via upload

---
+++ Example_Frame/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView i1,i2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        i1=(ImageView)findViewById(R.id.img1);
+        i2=(ImageView)findViewById(R.id.img2);
+        i1.setOnClickListener(this);
+        i2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.img1)
+        {
+            i1.setVisibility(View.GONE);
+            i2.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            i1.setVisibility(View.VISIBLE);
+            i2.setVisibility(View.GONE);
+        }
+    }
+}
